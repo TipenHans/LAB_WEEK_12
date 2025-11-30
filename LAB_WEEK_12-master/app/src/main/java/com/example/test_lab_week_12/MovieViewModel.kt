c3a38a20c6d6c0e6c7bf7d076f6125294ac2c206
TipenHans <steven190105@gmail.com>
2025-11-30 22:53:58 +0700

    Commit No. 2 – Building app with coroutines

--- LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -2,6 +2,22 @@ package com.example.test_lab_week_12
 
 import androidx.lifecycle.ViewModel
 
-class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
-
+class MovieViewModel(private val movieRepository: MovieRepository)
+    : ViewModel() {
+    init {
+        fetchPopularMovies()
+    }
+    // define the LiveData
+    val popularMovies: LiveData<List<Movie>>
+        get() = movieRepository.movies
+    val error: LiveData<String>
+        get() = movieRepository.error
+    // fetch movies from the API
+    private fun fetchPopularMovies() {
+// launch a coroutine in viewModelScope
+// Dispatchers.IO means that this coroutine will run on a shared pool of threads
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies()
+        }
+    }
 }
--- LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
+++ LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
@@ -1,4 +1,13 @@
 package com.example.test_lab_week_12.api
 
 interface MovieService {
+    @GET("movie/popular")
+// here, we are using the suspend keyword to indicate that this function is a coroutine
+// suspended functions can be paused and resumed at a later time
+// this is useful for network calls, since they can take a long time to complete
+// and we don't want to block the main thread
+// for more info, see: https://kotlinlang.org/docs/flow.html#suspending-functions
+    suspend fun getPopularMovies(
+        @Query("fd0e3f4ea0d72f78da718a81ce126b8c") apiKey: String
+    ): PopularMoviesResponse
 }
